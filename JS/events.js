0448e4ed9f198917b4e85091dde94037a0de555f
nayan-28 <nayanmalakar720@gmail.com>
2024-01-29 13:36:11 +0600

    Another Onclick Method

--- JS/events.js
+++ JS/events.js
@@ -1,1 +1,11 @@
-console.log("Amake ki peyecho??");
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.onclick = makeGreen;
+
+function makeGreen() {
+    document.body.style.backgroundColor = 'Green';
+}
+
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'Purple';
+}
