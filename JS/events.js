35c6e38ff1230aaa4841b4218a857c9ba0029a50
nayan-28 <nayanmalakar720@gmail.com>
2024-01-29 13:55:05 +0600

    addEventListener

--- JS/events.js
+++ JS/events.js
@@ -9,3 +9,28 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'Purple';
 }
+
+function makeRed() {
+    document.body.style.backgroundColor = 'Red';
+}
+
+const pinkButton = document.getElementById('make-pink');
+
+pinkButton.addEventListener('click', makePink)
+
+function makePink() {
+    document.body.style.backgroundColor = 'Pink';
+}
+
+
+const tomatoButton = document.getElementById('make-tomato');
+tomatoButton.addEventListener('click', function makeTomato() {
+    document.body.style.backgroundColor = 'Tomato';
+})
+
+
+/*Most useful*/
+
+document.getElementById('make-orange').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'Orange';
+})
